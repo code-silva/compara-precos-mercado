22988a38ea7b1174fa191026bc76c4d07ab63f93
Eduardo Waski <eduardowaski688@gmail.com>
2026-03-10 00:20:24 -0300

    Adicionando o serializer de ofertas_produto e criando o get para busca

--- backend/app_mercados/serializers.py
+++ backend/app_mercados/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
-from .models import Produto, MercadoFilial, Localidade, MercadoMatriz
+from .models import Produto, MercadoFilial, Produto_Oferta_Filial
 
 """
 o serializer transforma o que seria um campo com um ID e busca na tabela de onde o ID
 veio para retornar o valor que passamos e entrega no final um json
 """
+
+class OfertaProdutoSerializer(serializers.ModelSerializer):
+    produto_nome = serializers.ReadOnlyField(source='produto.nome')
+    produto_marca = serializers.ReadOnlyField(source='produto.marca')
+    produto_imagem = serializers.ImageField(source='produto.imagem', read_only=True)
+    mercado_nome = serializers.ReadOnlyField(source='mercado_filial.mercado_matriz.nome')
+    cidade = serializers.ReadOnlyField(source='mercado_filial.localidade.cidade')
+
+    class Meta:
+        model = Produto_Oferta_Filial
+        fields = ['id', 'preco', 'produto_nome', 'produto_marca', 'produto_imagem', 'mercado_nome', 'cidade']
 
 class ProdutoSerializer(serializers.ModelSerializer):
     categoria_nome = serializers.ReadOnlyField(source='categoria.nome')
--- backend/app_mercados/views.py
+++ backend/app_mercados/views.py
@@ -1,1 +1,20 @@
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Q
+from .models import Produto, MercadoFilial
+from .serializers import ProdutoSerializer, MercadoSerializer
+
+class BuscaHibridaView(APIView):
+    def get(self, request):
+        query = request.GET.get('q', '').strip()
+
+        if not query:
+            return Response({"produtos": [], "mercados": []})
+        
+        # busca por produto nos campos nome, marca, categoria
+        produtos = Produto.objects.filter(
+            Q(nome__icontains=query) |
+            Q(marca__icontains=query) |
+            Q(categoria__nome__icontains=query)
+        ).distinct
