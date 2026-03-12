467ea9301d4fa40b7d20ca1c52604663cbcdc509
Jose <josevictor21112005@gmail.com>
2026-03-12 01:06:06 -0300

    build(docker): adicionando orquestração com Docker Compose para o ambiente local
    - Criação docker-compose.yml para inicializar simultaneamente o frontend (Expo) e backend (Django).
    - Elaboração de Dockerfiles dedicados para cada serviço utilizando cache de camadas no Node.js e Python.
    - Isolação do ambiente de desenvolvimento, garantindo que o projeto seja executável em qualquer máquina sem atrito de dependências.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -87,7 +89,7 @@ DATABASES = {
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
-        "PORT": "5432",
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
@@ -122,8 +124,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
