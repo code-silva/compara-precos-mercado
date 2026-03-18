2c67ac118609573e3a2e8ce960ae7dc97f213ec9
Eduardo Silva Waski <eduardowaski688@gmail.com>
2026-03-18 00:50:10 -0300

    Add MercadoFilialListView URL pattern

--- backend/app_mercados/urls.py
+++ backend/app_mercados/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import BuscaHibridaView
+from .views import MercadoFilialListView
 
 urlpatterns = [
     path('busca/', BuscaHibridaView.as_view(), name='busca_hibrida'),
+    path("mercados-proximos/", MercadoFilialListView.as_view(), name="mercados-proximos"),
 ]
+
