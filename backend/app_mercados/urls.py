8982099eb633b04fee6dfec47244d33f934cbc48
Jose <josevictor21112005@gmail.com>
2026-04-29 23:19:47 -0300

    refactor: refatoração das variáveis no serializers.py e urls.py para garantir compatibilidade com o frontend
    - Tradução das chaves do JSON para o inglês e conversão do padrão snake_case para camelCase.

--- backend/app_mercados/urls.py
+++ backend/app_mercados/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import BuscaHibridaView, MercadoFilialListView, Produto_Oferta_FilialListView
 
 urlpatterns = [
-    path("busca/", BuscaHibridaView.as_view(), name="busca_hibrida"),
-    path("mercados-proximos/", MercadoFilialListView.as_view(), name="mercados-proximos"),
-    path("produtos/ofertas/", Produto_Oferta_FilialListView.as_view(), name="lista-ofertas"),
+    path("busca/", BuscaHibridaView.as_view(), name="search"),
+    path("nearby-markets/", MercadoFilialListView.as_view(), name="nearby_markets"),
+    path("products/offers/", Produto_Oferta_FilialListView.as_view(), name="offers_list"),
 ]
